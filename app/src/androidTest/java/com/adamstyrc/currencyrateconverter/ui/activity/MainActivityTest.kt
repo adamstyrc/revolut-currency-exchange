69014c973e8982f4a774990431781c4eb3673433
adamstyrc <styrc.adam@gmail.com>
2019-07-14 23:37:56 +0200

    Prepare for tests

---
+++ app/src/androidTest/java/com/adamstyrc/currencyrateconverter/ui/activity/MainActivityTest.kt
@@ -0,0 +1,5 @@
+package com.adamstyrc.currencyrateconverter.ui.activity
+
+import org.junit.Assert.*
+
+class MainActivityTest
---
+++ app/src/test/java/com/adamstyrc/currencyrateconverter/viewmodel/CurrencyRateViewModelTest.kt
@@ -0,0 +1,9 @@
+package com.adamstyrc.currencyrateconverter.viewmodel
+
+import org.junit.Assert.*
+
+// TODO testing of viewmodel
+// mock API with Dagger
+class CurrencyRateViewModelTest {
+
+}
