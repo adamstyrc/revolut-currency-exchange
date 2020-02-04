04edc161d173ecc8935f89b8265d0826c26c1ca0
astyrc <adam.styrc@vattenfall.com>
2020-02-04 23:30:55 +0100

    Remove old models

--- app/src/main/java/com/adamstyrc/currencyrateconverter/model/EstimatedCurrencyExchange.kt
+++
@@ -1,9 +0,0 @@
-package com.adamstyrc.currencyrateconverter.model
-
-import com.revolut.domain.model.Currency
-import java.math.BigDecimal
-
-//class EstimatedCurrencyExchange(
-//    var currency: Currency,
-//    var value: Money = BigDecimal.ZERO
-//)
--- app/src/main/java/com/adamstyrc/currencyrateconverter/model/Money.kt
+++
@@ -1,5 +0,0 @@
-package com.adamstyrc.currencyrateconverter.model
-
-import java.math.BigDecimal
-
-//typealias Money = BigDecimal
