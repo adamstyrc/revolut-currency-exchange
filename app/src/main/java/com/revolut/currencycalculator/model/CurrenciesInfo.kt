1efa8192fc2903c3aec1315e1769faefa7aa3f1b
astyrc <adam.styrc@vattenfall.com>
2020-02-05 22:58:29 +0100

    Add icons and names for Currencies p2

--- app/src/main/java/com/revolut/currencycalculator/model/CurrenciesInfo.kt
+++ app/src/main/java/com/revolut/currencycalculator/model/CurrenciesInfo.kt
@@ -8,13 +8,14 @@ object CurrenciesInfo {
     private val currencyToIcon = hashMapOf(
         Currency.USD to CurrencyInfo(R.drawable.ic_usd, R.string.currency_usd),
         Currency.EUR to CurrencyInfo(R.drawable.ic_eur, R.string.currency_eur),
-        Currency.CHF to CurrencyInfo(R.drawable.ic_chf, R.string.currency_chf),
-        Currency.AUD to CurrencyInfo(R.drawable.ic_aud, R.string.currency_aud),
-        Currency.CAD to CurrencyInfo(R.drawable.ic_cad, R.string.currency_cad),
         Currency.RUB to CurrencyInfo(R.drawable.ic_rub, R.string.currency_rub),
-        Currency.SEK to CurrencyInfo(R.drawable.ic_sek, R.string.currency_sek),
-        Currency.PLN to CurrencyInfo(R.drawable.ic_pln, R.string.currency_pln),
         Currency.GBP to CurrencyInfo(R.drawable.ic_gbp, R.string.currency_gbp),
+        Currency.PLN to CurrencyInfo(R.drawable.ic_pln, R.string.currency_pln),
+        Currency.AUD to CurrencyInfo(R.drawable.ic_aud, R.string.currency_aud),
+        Currency.BGN to CurrencyInfo(R.drawable.ic_bgn, R.string.currency_bgn),
+        Currency.BRL to CurrencyInfo(R.drawable.ic_brl, R.string.currency_brl),
+        Currency.CAD to CurrencyInfo(R.drawable.ic_cad, R.string.currency_cad),
+        Currency.CHF to CurrencyInfo(R.drawable.ic_chf, R.string.currency_chf),
         Currency.CZK to CurrencyInfo(R.drawable.ic_czk, R.string.currency_czk),
         Currency.CNY to CurrencyInfo(R.drawable.ic_cny, R.string.currency_cny),
         Currency.DKK to CurrencyInfo(R.drawable.ic_dkk, R.string.currency_dkk),
@@ -27,8 +28,17 @@ object CurrenciesInfo {
         Currency.ISK to CurrencyInfo(R.drawable.ic_isk, R.string.currency_isk),
         Currency.JPY to CurrencyInfo(R.drawable.ic_jpy, R.string.currency_jpy),
         Currency.KRW to CurrencyInfo(R.drawable.ic_krw, R.string.currency_krw),
-        Currency.MXN to CurrencyInfo(R.drawable.ic_mxn, R.string.currency_mxn)
-
+        Currency.MXN to CurrencyInfo(R.drawable.ic_mxn, R.string.currency_mxn),
+        Currency.MYR to CurrencyInfo(R.drawable.ic_myr, R.string.currency_myr),
+        Currency.NOK to CurrencyInfo(R.drawable.ic_nok, R.string.currency_nok),
+        Currency.NZD to CurrencyInfo(R.drawable.ic_nzd, R.string.currency_nzd),
+        Currency.PHP to CurrencyInfo(R.drawable.ic_php, R.string.currency_php),
+        Currency.RON to CurrencyInfo(R.drawable.ic_ron, R.string.currency_ron),
+        Currency.SEK to CurrencyInfo(R.drawable.ic_sek, R.string.currency_sek),
+        Currency.SGD to CurrencyInfo(R.drawable.ic_currency_unknown, R.string.currency_sgd),
+        Currency.THB to CurrencyInfo(R.drawable.ic_currency_unknown, R.string.currency_thb),
+        Currency.TRY to CurrencyInfo(R.drawable.ic_currency_unknown, R.string.currency_try),
+        Currency.ZAR to CurrencyInfo(R.drawable.ic_currency_unknown, R.string.currency_zar)
     )
 
     fun getIcon(currency: Currency): Int?
